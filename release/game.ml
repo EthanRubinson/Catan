f5b767c12c6babae33cc7dc7754bc7e5780fe457
Kate Boxer <ksb93@cornell.edu>
2014-05-05 21:58:05 +0000

    Update game.ml

--- release/game.ml
+++ release/game.ml
@@ -39,48 +39,43 @@ let handle_move s m =
 	let (intersList, rdList) = strctures in
 	match m with
 	|InitialMove(p1,p2) -> print_endline "InitialMove";
-			(
-				if (valid_town_spot intersList p1) then 
-				( (**THE INTERSECTION IS OPEN FOR A TOWN**) print_endline "Valid_Intersection";
-					let newInterList = setIthEleSet intersList p1 Town tn.active in
-					if (adacentpoints p1 p2 && (roadDoesExist rdList p1 p2 = false)) then
+    (let p1 = (if (valid_town_spot intersList p1) then (print_endline "Valid_Intersection"; p1) 
+      else (print_endline "Valid_Intersection"; list_indexof (fun x -> match x with |None -> true |_-> false) intersList )) in
+		
+					let newInterList = setIthEleSet intersList p1 Town tn.active  in 
+					if ((adacentpoints p1 p2) && (valid_road_position rdList p1 p2 )) then
 					(**ROAD IS VALID MOVE**)
 					( print_endline "Valid_road";
             let newrd = (tn.active,(p1,p2))::rdList in
-						(None,((((hex,port),(newInterList, newrd),dck, discd, robber),(update_resources pLst tn.active newInterList newrd p1 hex), tn, update_next tn),gi))
+						(None,((((hex,port),(newInterList, newrd),dck, discd, robber),(update_resources pLst tn.active newInterList newrd p1 hex), 
+          update_next_turn_color tn newrd, update_next tn newrd),gi))
 					)
 					else (**ROAD IS NOT VALID**)
 					(
               print_endline "inValid_road";
               let newrd = rdList@(random_road p1 rdList tn.active) in
-						 (None,((((hex,port),(newInterList,newrd),dck, discd, robber),(update_resources pLst tn.active newInterList newrd p1 hex), tn, update_next tn),gi))
-					)		
-				)
-				else (**THE INTERSECTION IS ALREADY TAKEN**) 
-				( print_endline "invalid_intersection";
-					let p1 = list_indexof (fun x -> match x with |None -> true |_-> false) intersList in
-          let newInterList = setIthEleSet intersList p1 Town tn.active in
-          if (adacentpoints p1 p2 && (roadDoesExist rdList p1 p2 = false)) then
-          (**ROAD IS VALID MOVE**)
-          (
-            print_endline "Valid_road";
-            let newrd = (tn.active,(p1,p2))::rdList in
-            (None,((((hex,port),(newInterList, newrd),dck, discd, robber),(update_resources pLst tn.active newInterList newrd p1 hex), tn, update_next tn),gi))
-          )
-          else (**ROAD IS NOT VALID**)
-          (
-            print_endline "inValid_road";
-              let newrd = rdList@(random_road p1 rdList tn.active) in
-             (None,((((hex,port),(newInterList,newrd),dck, discd, robber),(update_resources pLst tn.active newInterList newrd p1 hex), tn, update_next tn),gi))
-          )   
-				)
-
+						 (None,((((hex,port),(newInterList,newrd),dck, discd, robber),(update_resources pLst tn.active newInterList newrd p1 hex), 
+            update_next_turn_color tn newrd, update_next tn newrd),gi))
+					))
 			
-			)
+			
+			
 	|RobberMove(rm) -> (None, s)
 	|DiscardMove(cost1) -> (None, s)
 	|TradeResponse(yesno) -> (None, s)
-	|Action(a) -> (None, s) 
+	|Action(a) -> print_endline "action move";
+    match a with
+    |RollDice -> print_endline "dice roll";
+      let dr = random_roll () in 
+      if (dr = cROBBER_ROLL) then ((None,((((hex,port),strctures,dck, discd, robber), pLst, tn, (tn.active, DiscardRequest)),gi))) 
+      else 
+      (None, ((((hex,port),strctures,dck, discd, robber), 
+        update_resources_playerlist dr pLst intersList hex, tn, (tn.active, DiscardRequest)),gi))
+    |MaritimeTrade (m) -> (None, s)
+    |DomesticTrade (d)-> (None, s)
+    |BuyBuild (b)-> (None, s)
+    |PlayCard (pc)-> (None, s)
+    |EndTurn -> (None, s)
 
 
 let presentation g =  
