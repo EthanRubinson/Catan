3d3277c83432c78782ef1527bae6b2b9920f15ac
Kate Boxer <ksb93@cornell.edu>
2014-05-05 16:34:28 +0000

    Update game.ml

--- release/game.ml
+++ release/game.ml
@@ -33,28 +33,45 @@ let game_of_state s =
 let init_game () = game_of_state (gen_initial_state())
 
 
-let handle_move s m = failwith "testing"
-	(**let ((mp,strctures,dck, discd, robber),pLst, tn, nxt) = s in
+let handle_move s m =
+  let ((((hex,port),strctures,dck, discd, robber),pLst, tn, nxt),gi) = s in
 	let (intersList, rdList) = strctures in
 	match m with
-	|InitialMove(p1,p2) -> 
+	|InitialMove(p1,p2) -> print_endline "InitialMove";
 			(
-				if (ithEleNone intersList p1) then 
-				( (**THE INTERSECTION IS OPEN FOR A TOWN**)
+				if (valid_town_spot intersList p1) then 
+				( (**THE INTERSECTION IS OPEN FOR A TOWN**) print_endline "Valid_Intersection";
 					let newInterList = setIthEleSet intersList p1 Town tn.active in
 					if (adacentpoints p1 p2 && (roadDoesExist rdList p1 p2 = false)) then
 					(**ROAD IS VALID MOVE**)
-					(
-						(None,((mp,(newInterList,(tn.active,(p1,p2))::rdList),dck, discd, robber),pLst, tn, nxt))
+					( print_endline "Valid_road";
+            let newrd = (tn.active,(p1,p2))::rdList in
+						(None,((((hex,port),(newInterList, newrd),dck, discd, robber),(update_resources pLst tn.active newInterList newrd p1 hex), tn, update_next tn),gi))
 					)
-					else
+					else (**ROAD IS NOT VALID**)
 					(
-						failwith "not yet implemented"
+              print_endline "inValid_road";
+              let newrd = rdList@(random_road p1 rdList tn.active) in
+						 (None,((((hex,port),(newInterList,newrd),dck, discd, robber),(update_resources pLst tn.active newInterList newrd p1 hex), tn, update_next tn),gi))
 					)		
 				)
-				else (**THE INTERSECTION IS ALREADY TAKEN**)
-				(
-					failwith "not htere yet"
+				else (**THE INTERSECTION IS ALREADY TAKEN**) 
+				( print_endline "invalid_intersection";
+					let p1 = list_indexof (fun x -> match x with |None -> true |_-> false) intersList in
+          let newInterList = setIthEleSet intersList p1 Town tn.active in
+          if (adacentpoints p1 p2 && (roadDoesExist rdList p1 p2 = false)) then
+          (**ROAD IS VALID MOVE**)
+          (
+            print_endline "Valid_road";
+            let newrd = (tn.active,(p1,p2))::rdList in
+            (None,((((hex,port),(newInterList, newrd),dck, discd, robber),(update_resources pLst tn.active newInterList newrd p1 hex), tn, update_next tn),gi))
+          )
+          else (**ROAD IS NOT VALID**)
+          (
+            print_endline "inValid_road";
+              let newrd = rdList@(random_road p1 rdList tn.active) in
+             (None,((((hex,port),(newInterList,newrd),dck, discd, robber),(update_resources pLst tn.active newInterList newrd p1 hex), tn, update_next tn),gi))
+          )   
 				)
 
 			
@@ -62,7 +79,7 @@ let handle_move s m = failwith "testing"
 	|RobberMove(rm) -> (None, s)
 	|DiscardMove(cost1) -> (None, s)
 	|TradeResponse(yesno) -> (None, s)
-	|Action(a) -> (None, s) **)
+	|Action(a) -> (None, s) 
 
 
 let presentation g =  
@@ -79,43 +96,4 @@ let presentation g =
   let new_state = (board, new_player_list, turn, next) in
     game_of_state new_state 
   
-  (**EXTRA METHODS --- KATE **)
-
-  (**return true if ith element is None, false otherwise**)
-let ithEleNone lst p1 = 
-  let rec ithhelper lst i = 
-  match lst with
-  [] -> false
-  |h::t -> if (i = p1) then
-          (match h with
-          |None -> true
-          |_-> false) else ithhelper t (i+1) in
-  ithhelper lst 0
-
-  (**sets ith element in intersect list to settlement**)
-  let setIthEleSet lst p1 set c=
-    let rec ithhelper lst i acc=
-    match lst with
-    [] ->  acc
-    |h::t -> if (i = p1) then ithhelper t (i+1) acc@[Some(c,set)] else ithhelper t (i+1) acc@t in
-    ithhelper lst 0 []
-
-
-(**checks if point is adacent to another point**)
-  let adacentpoints p1 p2 =
-    let adList = (adjacent_points p1) in
-    List.mem p2 adList
-
-(** checks if road does not exist true if does false if not**)  
-  let roadDoesExist rdList p1 p2 = 
-    let rec roadhelper rdList p1 p2 = 
-    match rdList with
-    [] -> false
-    |(c,(p3,p4))::t -> 
-    (
-      if ((p3 = p1 && p4 = p2) || (p3 = p2 && p4 = p1)) then true else roadhelper t p1 p2
-    )
-  in roadhelper rdList p1 p2
-
-
   
